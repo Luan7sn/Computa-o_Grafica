d6ed16017173c2343caf83c4bfab58cc0eff0cd3
Luan7sn <37909900+Luan7sn@users.noreply.github.com>
2019-05-23 15:43:30 -0300

    Update revolucao.py

--- Trabalho - 3/revolucao.py
+++ Trabalho - 3/revolucao.py
@@ -3,79 +3,58 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 from math import *
 from random import *
-
-#(r*cosA*cosP, r*senA*senP, r*cosA*senP)
-
-r = 6
-
-alpha = -pi/2
-phi = 0
-
-xCirculo = r*cos(alpha)*cos(phi)
-yCirculo = r*sin(alpha)*sin(phi)
-zCirculo = r*cos(alpha)*sin(phi)
-
-a = 0.9
-b = 0.9
-
-x0 = -2
-y0 = -2
-
-xf = 2
-yf = 2
-
-px = 10
-py = 10
-vertex = []
+from numpy import *
 
 cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
+corBase = ((1,1,1))
+corTopo = ((0.5,0.5,0.5))
 
-def revolucao():
-    i = 0
-    j = 0
-    dx = (xf -x0)/px
-    dy = (yf -y0)/py
-    y = y0
-    glPushMatrix()
-    glTranslatef(0,0,8)
-    while(y <= yf):
-        x = x0
-        glBegin(GL_QUAD_STRIP)
-        while(x <= xf):
-            glColor3f(uniform(0,1), uniform(0,1), uniform(0,1))
-            glVertex3f(x, y, f(x,y))
-            glColor3f(uniform(0,1), uniform(0,1), uniform(0,1))
-            glVertex3f(x, y+dy, f(x, y+dy))
-            x += dx
-        y += dy
-        glEnd()
-    y = y0
-    glPopMatrix()
-    glPushMatrix()
-    glTranslatef(0,0,-8)
-    while(y <= yf):
-        x = x0
-        glBegin(GL_QUAD_STRIP)
-        while(x <= xf):
-            glColor3f(uniform(0,1), uniform(0,1), uniform(0,1))
-            glVertex3f(x,y,f2(x,y))
-            glColor3f(uniform(0,1), uniform(0,1), uniform(0,1))
-            glVertex3f(x,y+dy,f2(x,y+dy))
-            x += dx
-        y += dy
-        glEnd()
-    glPopMatrix()
+x = 0
+y = 0
+z = 0
+pontos = []
+r = 10
+i = 0
+l = 3
+o = -0.5
+p = 0
 
-def f2 (x,y):
-    return (a*x)**2 + (b*y)**2
+#for o in arange(-0.5, 0.5, 0.01):
+while(o < 0.5):
+    p = 0
+    #for p in arange(0, 2, 0.02):
+    while(p < 2):
+        x = r*cos(pi*o)*cos(pi*p)
+        y = r*sin(pi*o)
+        z = r*cos(pi*o)*sin(pi*p)
+        pontos.insert(i, (x,y,z))
+        i+= 1
+        p += 0.2/l
+    o += 0.1/l
 
-def f (x,y):
-    return -(a*x)**2 + -(b*y)**2
+def rev():
+    glBegin(GL_TRIANGLES)
+    for i in range(0,len(pontos)):
+        if i >= len(pontos)-((10*l)+1):
+            break
+       
+        glColor3f(0.5,1,1)
+        #glVertex3fv(pontos[i*l], pontos[i+(1*l)], pontos[i+(10*l)])
+        #glVertex3fv(pontos[i+(10*l)], pontos[i+(11*l)], pontos[i+(1*l)])
+
+        glVertex3fv(pontos[i])
+        glVertex3fv(pontos[i+1])
+        glVertex3fv(pontos[i+(10*l)])
+        glColor3f(0.5,0.5,1)
+        glVertex3fv(pontos[i+(10*l)])
+        glVertex3fv(pontos[i+((10*l)+1)])
+        glVertex3fv(pontos[i+1])
+    glEnd()
 
 def abacaxi():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glRotatef(1,0,1,0)
-    revolucao()
+    glRotatef(200,0,0,0)
+    rev()
     glutSwapBuffers()
  
 def timer(i):
@@ -85,14 +64,14 @@ def timer(i):
 # PROGRAMA PRINCIPAL
 glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
-glutInitWindowSize(800,800)
-glutCreateWindow("REVOLUCAO DA HORA")
+glutInitWindowSize(1000,1000)
+glutCreateWindow("Rev")
 glutDisplayFunc(abacaxi)
 glEnable(GL_MULTISAMPLE)
 glEnable(GL_DEPTH_TEST)
 glClearColor(0.,0.,0.,1.)
-gluPerspective(50,800.0/600.0,0.1,50.0)
+gluPerspective(100,800.0/800.0,0.1,50.0)
 glTranslatef(0.0,0.0,-20)
-#glRotatef(45,0,0,0)
+glRotatef(45,0,0,0)
 glutTimerFunc(50,timer,1)
 glutMainLoop()
